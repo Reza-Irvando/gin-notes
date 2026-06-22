29d26031e105fc8694d2e671e72794ce408ff071
Pando <rezairvando@gmail.com>
2026-06-22 21:54:31 +0700

    add comment config

--- configs/config.go
+++ configs/config.go
@@ -5,8 +5,10 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
+// Variabel global untuk database
 var DB *gorm.DB
 
+// Fungsi untuk inisialisasi koneksi database
 func InitDB() (*gorm.DB, error) {
 	db, err := gorm.Open("mysql", "root:@tcp(127.0.0.1:3306)/go-notes?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
