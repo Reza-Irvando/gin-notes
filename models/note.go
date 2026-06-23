d70cf8e2c26cd1a4a673cc87f0f134141b4df105
Pando <rezairvando@gmail.com>
2026-06-23 18:20:57 +0700

    feat: add CategoryID and DeletedAt fields to Note model

--- models/note.go
+++ models/note.go
@@ -4,11 +4,13 @@ import "time"
 
 // Model Note
 type Note struct {
-	ID        uint      `gorm:"primaryKey" json:"id"`
-	UserID    uint      `gorm:"not null;index" json:"user_id"`
-	Title     string    `gorm:"not null" json:"title" binding:"required"`
-	Content   string    `gorm:"type:text;not null" json:"content" binding:"required"`
-	Status    string    `gorm:"type:enum('Active','Archived');default:'Active'" json:"status"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID         uint       `gorm:"primaryKey" json:"id"`
+	UserID     uint       `gorm:"not null;index" json:"user_id"`
+	CategoryID *uint      `gorm:"index" json:"category_id"`
+	Title      string     `gorm:"not null" json:"title" binding:"required"`
+	Content    string     `gorm:"type:text;not null" json:"content" binding:"required"`
+	Status     string     `gorm:"type:enum('Active','Archived');default:'Active'" json:"status"`
+	CreatedAt  time.Time  `json:"created_at"`
+	UpdatedAt  time.Time  `json:"updated_at"`
+	DeletedAt  *time.Time `gorm:"index" json:"deleted_at"`
 }
